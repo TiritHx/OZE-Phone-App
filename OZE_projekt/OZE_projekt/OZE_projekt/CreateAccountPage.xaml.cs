de5a7d5a9ea41d4c63ce3432c41a064454441dec
Dominik <90091417+TiritHx@users.noreply.github.com>
2023-04-03 14:54:07 +0200

    Przeniesiono pliki z komputera szkolnego
    W obecnym stanie aplikacja ma 2 strony, główną logowania i od tworzenia konta, walidacja haseł została uwzględniona

---
+++ OZE_projekt/OZE_projekt/OZE_projekt/CreateAccountPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OZE_projekt
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateAccountPage : ContentPage
+    {
+        public CreateAccountPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Validate(object sender, EventArgs e)
+        {
+            if (password.Text == repeat_password.Text)
+            {
+                CreateAccountButton.IsEnabled = true;
+                CreateAccountButton.Text = "Utwórz nowe konto";
+            }
+            else
+            {
+                CreateAccountButton.IsEnabled = false;
+                CreateAccountButton.Text = "Hasła nie są takie same";
+            }
+        }
+
+        private void CreateAccount_Button_Clicked(object sender, EventArgs e)
+        {
+            //Trza zrobić walidacje powtórzone hasła, jeśli git to dodaj konto do bazy danych
+            if(password.Text == repeat_password.Text)
+            {
+
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
---
+++ OZE_projekt/OZE_projekt/OZE_projekt/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OZE_projekt
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NewAccount_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CreateAccountPage());
+        }
+
+        
+    }
+}
